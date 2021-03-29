a8b08314db6a8f41ac39e94be2bd902b0fb51a12
Cathrina <cathrina.hao@unity3d.com>
2021-03-29 16:12:36 +0900

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Cathrina Hao and is {""}
-        <a href="https://github.com/cathrinahwj/react-project" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Cathrina Hao and is {""}
+          <a
+            href="https://github.com/cathrinahwj/react-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
