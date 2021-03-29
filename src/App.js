c59af4e80da4fbaae16c705c66dce4a2a7d28a00
Cathrina <cathrina.hao@unity3d.com>
2021-03-29 15:37:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Cathrina Hao and is {""}
+        <a href="https://github.com/cathrinahwj/react-project" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
