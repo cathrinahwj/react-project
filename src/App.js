ce0e03f0ea259fb7e1034f0b5f87d15da35d6fc5
Cathrina <cathrina.hao@unity3d.com>
2021-03-29 20:58:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ export default function App() {
         <Weather />
         <footer>
           This project was coded by Cathrina Hao and is {""}
-          <span
+          <a
             href="https://github.com/cathrinahwj/react-project"
             target="_blank"
           >
             open-sourced on GitHub
-          </span>
+          </a>
         </footer>
       </div>
     </div>
